2a7641c342d198930089e774940d81d5bf2209f4
ali nikookherad <ali.nikookherad.abrha@gmail.com>
2025-05-17 20:02:05 +0330

    add docker sail

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\ObserverServiceProvider::class,
     App\Providers\RelationshipServiceProvider::class,
 ];
